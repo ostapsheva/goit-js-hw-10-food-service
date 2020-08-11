b5445d0c0a42f767c6619a97b925fd90047d3c0d
Ostap Shevchenko <ostapshev@gmail.com>
2020-08-12 02:11:22 +0300

    Add hw-10

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './styles.css';
+import menuData from './menu.json';
+import itemTemplate from './item.hbs';
+
+const refs = {
+  body: document.querySelector('body'),
+  input: document.querySelector('input.js-switch-input'),
+  menuList: document.querySelector('ul.js-menu'),
+};
+
+const items = itemTemplate(menuData);
+refs.menuList.insertAdjacentHTML('beforeend', items);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkThemeColor = function () {
+  if (localStorage.getItem('theme') === JSON.stringify(Theme.LIGHT)) {
+    refs.body.classList.add('light-theme');
+  } else if (localStorage.getItem('theme') === JSON.stringify(Theme.DARK)) {
+    refs.body.classList.add('dark-theme');
+    refs.input.checked = true;
+  }
+};
+checkThemeColor();
+
+const changeThemeColor = function (e) {
+  if (e.target.checked) {
+    refs.body.classList.remove('light-theme');
+    refs.body.classList.add('dark-theme');
+    localStorage.setItem('theme', JSON.stringify(Theme.DARK));
+  } else if (!e.target.checked) {
+    refs.body.classList.remove('dark-theme');
+    refs.body.classList.add('light-theme');
+    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
+  }
+};
+refs.input.addEventListener('click', changeThemeColor);
